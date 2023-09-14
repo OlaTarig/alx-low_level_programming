9552ca98eb0672df5b1a447d74996e51b545ca11
OlaTarig <olatarig190@gmail.com>
2023-09-14 12:39:34 -0700

    first task

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+/**
+ * _isupper - check for uppercase letters.
+ *@c: function parameter
+ * Return: 0 or 1.
+ */
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
